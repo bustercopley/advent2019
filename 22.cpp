9e61cdf0a9950b670d9ed40332f7f69ea9e47036
Buster <buster@buster.me.uk>
2019-12-22 14:14:18 +0000

    Day 22 part 2, exponent 1

--- 22.cpp
+++ 22.cpp
@@ -1,5 +1,5 @@
 #include "precompiled.h"
-#include <set>
+#include "egcd.h"
 
 enum method_t { cut, stack, increment };
 
@@ -10,60 +10,13 @@ struct technique_t {
 
 using techniques_t = std::vector<technique_t>;
 
-template <int64_t N> void part_one(const techniques_t &techniques, bool verbose) {
-  using deck_t = std::array<int64_t, N>;
-  deck_t deck;
-  for (int64_t i = 0; i != N; ++i) {
-    deck[i] = i;
-  }
-  for (auto [method, param] : techniques) {
-    switch (method) {
-    case cut:
-      if (verbose) {
-        std::cout << "cut " << param << std::endl;
-      }
-      if (param < 0) {
-        param += N;
-      }
-      std::rotate(&deck[0], &deck[param], &deck[N]);
-      break;
-    case stack:
-      if (verbose) {
-        std::cout << "deal int64_to new stack\n";
-      }
-      std::reverse(&deck[0], &deck[N]);
-      break;
-    case increment: {
-      if (verbose) {
-        std::cout << "deal with increment " << param << std::endl;
-      }
-      deck_t new_deck;
-      int64_t j = 0;
-      for (int64_t i = 0; i != N; ++i) {
-        new_deck[j] = deck[i];
-        j = (j + param) % N;
-      }
-      deck = new_deck;
-    } break;
-    }
-    if (verbose) {
-      for (auto card : deck) {
-        std::cout << " " << card;
-      }
-      std::cout << "\n";
-    }
-  }
-  if (!verbose) {
-    if (auto iter = std::find(&deck[0], &deck[N], 2019); iter != &deck[N]) {
-      std::cout << (iter - &deck[0]) << std::endl;
-    }
-  }
-}
-
 std::vector<technique_t> read_techniques(const char *filename) {
+  std::cout << "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
+  std::cout << "Read file \"" << filename << "\"" << std::endl;
   std::ifstream in(filename);
   std::map<std::string, std::string> orbits;
-  std::regex regex1("cut (-?\\d+)|deal int64_to new stack|deal with increment (\\d+)");
+  std::regex regex1(
+    "cut (-?\\d+)|deal into new stack|deal with increment (\\d+)");
   std::string line;
   techniques_t techniques;
   while (std::getline(in, line)) {
@@ -95,18 +48,173 @@ std::vector<technique_t> read_techniques(const char *filename) {
   return techniques;
 }
 
-void do_it() try {
-  for (auto filename : {"22-test.data"}) {
-    std::cout << "Processing " << filename << std::endl;
-    auto techniques = read_techniques(filename);
-    part_one<10>(techniques, true);
-    // part_two(techniques);
+int64_t apply_inverse(
+  technique_t technique, int64_t modulus, int64_t i, bool verbose) {
+  auto [method, param] = technique;
+  switch (method) {
+  case cut:
+    if (verbose) {
+      std::cout << "cut " << param << ": " << i;
+    }
+    i = (i + modulus + param) % modulus;
+    break;
+  case stack:
+    if (verbose) {
+      std::cout << "deal into new stack: " << i;
+    }
+    i = modulus - 1 - i;
+    break;
+  case increment: {
+    if (verbose) {
+      std::cout << "deal with increment " << param << ": " << i;
+    }
+    i = (int64_t)(((__int128_t)i * (__int128_t)modinv(param, modulus)) %
+                  (__int128_t)modulus);
+    break;
   }
-  for (auto filename : {"22.data"}) {
-    std::cout << "Processing " << filename << std::endl;
+  }
+  if (verbose) {
+    std::cout << " -> " << i << std::endl;
+  }
+  return i;
+}
+
+int64_t apply(technique_t technique, int64_t modulus, int64_t i, bool verbose) {
+  auto [method, param] = technique;
+  switch (method) {
+  case cut:
+    if (verbose) {
+      std::cout << "cut " << param << ": " << i;
+    }
+    i = (i + modulus - param) % modulus;
+    break;
+  case stack:
+    if (verbose) {
+      std::cout << "deal into new stack: " << i;
+    }
+    i = modulus - 1 - i;
+    break;
+  case increment: {
+    if (verbose) {
+      std::cout << "deal with increment " << param << ": " << i;
+    }
+    i = (int64_t)(((__int128_t)i * (__int128_t)param) % (__int128_t)modulus);
+    break;
+  }
+  }
+  if (verbose) {
+    std::cout << " -> " << i << std::endl;
+  }
+  return i;
+}
+
+void naive(const techniques_t &techniques, int64_t modulus, int64_t value,
+  bool verbose) {
+  std::cout << "------------------------------------------------------------\n"
+            << "Naive, modulus " << modulus << ", value " << value << std::endl;
+  using deck_t = std::vector<int64_t>;
+  deck_t deck(modulus);
+  for (int64_t i = 0; i != modulus; ++i) {
+    deck[i] = i;
+  }
+  if (verbose) {
+    for (auto card : deck) {
+      std::cout << " " << card;
+    }
+    std::cout << "\n";
+  }
+  for (auto [method, param] : techniques) {
+    switch (method) {
+    case cut:
+      if (verbose) {
+        std::cout << "cut " << param << std::endl;
+      }
+      if (param < 0) {
+        param += modulus;
+      }
+      std::rotate(&deck[0], &deck[param], &deck[modulus]);
+      break;
+    case stack:
+      if (verbose) {
+        std::cout << "deal into new stack\n";
+      }
+      std::reverse(&deck[0], &deck[modulus]);
+      break;
+    case increment: {
+      if (verbose) {
+        std::cout << "deal with increment " << param << std::endl;
+      }
+      deck_t new_deck(modulus);
+      int64_t j = 0;
+      for (int64_t i = 0; i != modulus; ++i) {
+        new_deck[j] = deck[i];
+        j = (j + param) % modulus;
+      }
+      deck = new_deck;
+    } break;
+    }
+    if (verbose) {
+      for (auto card : deck) {
+        std::cout << " " << card;
+      }
+      std::cout << "\n";
+    }
+  }
+}
+
+int64_t apply_inverse(
+  const techniques_t &techniques, int64_t modulus, int64_t i, bool verbose) {
+  for (auto iter = std::rbegin(techniques); iter != std::rend(techniques);
+       ++iter) {
+    i = apply_inverse(*iter, modulus, i, verbose);
+  }
+  return i;
+}
+
+int64_t apply(
+  const techniques_t &techniques, int64_t modulus, int64_t i, bool verbose) {
+  for (const auto &technique : techniques) {
+    i = apply(technique, modulus, i, verbose);
+  }
+  return i;
+}
+
+void part_one(const techniques_t &techniques, int64_t modulus, int64_t value,
+  bool verbose) {
+  std::cout << "------------------------------------------------------------\n"
+            << "Part one, modulus " << modulus << ", value " << value
+            << std::endl;
+  int64_t i = apply(techniques, modulus, value, verbose);
+  std::cout << "Position of card " << value << " is " << i << std::endl;
+}
+
+void part_two_exponent_one(const techniques_t &techniques, int64_t modulus, int64_t position,
+  bool verbose) {
+  std::cout << "------------------------------------------------------------\n"
+            << "Part two, modulus " << modulus << ", position " << position
+            << ", exponent 1" << std::endl;
+  int64_t i = apply_inverse(techniques, modulus, position, verbose);
+  std::cout << "Card in position " << position << " is " << i << std::endl;
+}
+
+void do_it() try {
+  for (auto filename :
+    {"22-test-1.data", "22-test-2.data", "22-test-3.data", "22-test-4.data"}) {
     auto techniques = read_techniques(filename);
-    part_one<10007>(techniques, false);
-    //part_two<119315717514047>(techniques, false); :)
+    naive(techniques, 10, 3, true);
+    part_one(techniques, 10, 3, true);
+    part_two_exponent_one(techniques, 10, 3, true);
+  }
+  {
+    auto techniques = read_techniques("22.data");
+    // Part one, modulus 10007, value 2019, position ?
+    part_one(techniques, 10007, 2019, false);
+
+    // Part two,
+    // modulus 119315717514047, prime, 2 ** 46.76177743085247
+    // position 2020
+    // exponent 101741582076661, prime, 2 ** 46.53190276174851
+    part_two_exponent_one(techniques, 119315717514047, 2020, false);
   }
 } catch (const char *e) {
   std::cout << e << std::endl;
